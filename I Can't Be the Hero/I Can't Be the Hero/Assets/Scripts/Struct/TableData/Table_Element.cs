a59b70d9046606e1250b0ef925572a51268190c0
NDDYLYS <nodvic@naver.com>
2026-09-13 19:20:21 +0900

    ADD

---
+++ I Can't Be the Hero/I Can't Be the Hero/Assets/Scripts/Struct/TableData/Table_Element.cs
@@ -0,0 +1,42 @@
+﻿
+[System.Serializable]
+public class Table_Element
+{
+	public int Index ;
+	public string CodeName ;
+	public int[] ElementDamage ;
+
+
+	public static void AutoLoadTable()
+	{
+		string[,] data = TableDataManager.Instance.PublicExcelReader("Table_Element", true);
+		LoadTable(data);
+	}
+
+
+	public static void LoadTable(string[,] _data)
+	{
+		for (int i = 4; i < _data.GetLength(0); i++)
+		{
+			int columnCount = 0;
+			Table_Element newData = new Table_Element();
+			newData.Index = int.Parse(_data[i, columnCount++]);
+			newData.CodeName = _data[i, columnCount++];
+			newData.ElementDamage = new int[11];
+			newData.ElementDamage[0] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[1] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[2] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[3] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[4] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[5] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[6] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[7] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[8] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[9] = int.Parse(_data[i, columnCount++]);
+			newData.ElementDamage[10] = int.Parse(_data[i, columnCount++]);
+			TableDataManager.Instance.SetDictinary<Table_Element>(newData.Index, newData.CodeName, newData);
+		}
+	}
+
+
+}
---
+++ I Can't Be the Hero/I Can't Be the Hero/Assets/Scripts/Utils/Util.battle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+public static partial class Util
+{
+    //public static void CreateMessageBox(string _codename)
+    //{
+    //    var prefab = UIPrefabManager.Instance.MessageBoxProperty.OnClickOpenPageButton(_codename);
+    //}
+
+    public static int BattleElementDamage(ElementEnum _defender, ElementEnum _attacker) 
+    {
+        var damage = 0;
+        var defender = _defender;
+        var defEl = TableDataManager.Instance.GetTableData<Table_Element>(_defender.ToString());
+        var attacker = _attacker;
+        damage = defEl.ElementDamage[(int)attacker];
+
+        LogManager.Instance.DebugLogCategory(LogCategoryEnum.Battle, $"{defender}/{attacker}>>>{damage}");
+
+        return damage;
+    }
+}
